c159e1169e6d72912fce1a40c9e61072a66eb49a
Ferid HELALI <helaliferid@gmail.com>
2022-02-14 13:05:15 +0100

    fix convas context drowing

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { map } from 'rxjs'
 // get The Canvas node
 const canvas=document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
+const radius = 12
 
 
 
@@ -23,11 +24,9 @@ source$.pipe(
         next: point =>{
             console.log(point)
             ctx.beginPath();
-            ctx.strokeStyle = 'black';
-            ctx.lineWidth = 5;
-            ctx.moveTo(point.x, point.y);
-            ctx.arcTo(point.x,point.y, 2,10,10);
-            ctx.stroke();
+            ctx.fillStyle = 'blue';
+            ctx.arc(point.x, point.y, radius, 0 * Math.PI, 2 * Math.PI);
+            ctx.fill();
            
         }
     })  
